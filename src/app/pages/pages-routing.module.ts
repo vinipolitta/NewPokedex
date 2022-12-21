f15baf0044858fbb7d6651b3ff0db3f12055c2a9
Vinicius Politta <vinicius.politta1@gmail.com>
2022-12-21 10:59:19 -0300

    first commit

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,12 @@
+import { DetailsComponent } from './details/details.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'details/:id', component: DetailsComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
